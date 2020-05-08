8f3c0eb8ef2a10687da28c3af3acb81461a5d529
Bruno DaSilva <hi.dasilvab@gmail.com>
2020-05-08 15:35:56 -0500

    fixed nav links

--- views/Show.jsx
+++ views/Show.jsx
@@ -29,12 +29,12 @@ class Show extends React.Component {
           <div className="nav-bar">
             <ul>
               <li>
-                <a href="http://localhost:3000/budgets">
+                <a href="/budgets">
                   <i className="fas fa-home "></i>
                 </a>
               </li>
               <li>
-                <a href="http://localhost:3000/budgets/new">
+                <a href="/budgets/new">
                   <i className="fas fa-plus "></i>
                 </a>
               </li>
